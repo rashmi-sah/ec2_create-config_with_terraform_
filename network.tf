1ed01bb5c18ab257e3aa9d9fad89327e4a196c9d
rashmi sah <105689930+rashmi-sah@users.noreply.github.com>
2022-09-15 15:28:07 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -13,6 +13,15 @@
    cidr_block = "${var.public_subnets}"        # CIDR block of public subnets
  }
 
+#Route table for Public Subnet's
+ resource "aws_route_table" "PublicRT" {    # Creating RT for Public Subnet
+    vpc_id =  aws_vpc.Main.id
+         route {
+    cidr_block = "0.0.0.0/0"               # Traffic from Public Subnet reaches Internet via Internet Gateway
+    gateway_id = aws_internet_gateway.IGW.id
+     }
+ }
+
 #Route table Association with Public Subnet's
  resource "aws_route_table_association" "PublicRTassociation" {
     subnet_id = aws_subnet.publicsubnets.id
