cc46ab17b48616a876119f4f94b30784ad87bfe1
rashmi sah <105689930+rashmi-sah@users.noreply.github.com>
2022-09-15 15:21:38 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,6 +1,7 @@
 
 variable "region" {}
- variable "main_vpc_cidr" {}
- variable "public_subnets" {}
- variable "private_subnets" {}
+ variable "main_vpc_cidr" {"10.0.0.0/24"}
+ variable "public_subnets" {"10.0.0.128/26"}
+ variable "private_subnets" {"10.0.0.192/26"}
+ 
  
