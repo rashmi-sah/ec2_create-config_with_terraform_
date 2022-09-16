26879d47650f85537d9e34c77a8f3d869c627359
rashmi sah <105689930+rashmi-sah@users.noreply.github.com>
2022-09-16 08:23:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ variable "awsprops" {
     type = "map"
     default = {
     region = "us-east-1"
-    vpc = aws_vpc.Main.id
+    #vpc = aws_vpc.Main.id
     ami = "ami-052efd3df9dad4825"
     itype = "t2.micro"
-    subnet = aws_subnet.publicsubnets.id
+    #subnet = aws_subnet.publicsubnets.id
     publicip = true
     keyname = "AWS_EC2_key"
     secgroupname = "terraform-Sec-Group"
