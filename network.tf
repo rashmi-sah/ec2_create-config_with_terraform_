0874e0e4f17804b17fba86d20abdf3a58edd6a58
rashmi sah <105689930+rashmi-sah@users.noreply.github.com>
2022-09-19 23:44:49 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,4 +1,4 @@
-#Create the VPC
+# Create the VPC , subnet, gateways, routetable, security list
  resource "aws_vpc" "Main" {                # Creating VPC here
    cidr_block       = var.main_vpc_cidr     # Defining the CIDR block use 10.0.0.0/24 for demo
    instance_tenancy = "default"
